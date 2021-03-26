706e7b95b2e37f16bb54d3b6eb1a9e3b60f86e01
Trond Engell <trond.engell@gmail.com>
2021-03-26 14:31:59 +0100

    Update documentation
    Remove inconsistencies.

--- src/main/kotlin/no/liflig/properties/Properties.kt
+++ src/main/kotlin/no/liflig/properties/Properties.kt
@@ -3,8 +3,7 @@ package no.liflig.properties
 import java.util.Properties
 
 /**
- * Returns the optional string value of the property given by [key].
- * returns null if the property does not exist.
+ * Returns the optional string value of the property given by [key] or null if the property does not exist.
  */
 fun Properties.string(key: String): String? = getProperty(key)
 
@@ -32,7 +31,7 @@ fun Properties.stringNotEmpty(key: String): String =
 /**
  * Returns the integer value of the property given by [key]
  * or null if property does not exist or if string value is blank.
- * @throws IllegalArgumentException if the value is not a valid representation of a number.
+ * @throws IllegalArgumentException if the value is not a valid representation of an integer.
  */
 @JvmName("getInt")
 fun Properties.int(key: String): Int? =
@@ -48,15 +47,16 @@ fun Properties.int(key: String): Int? =
 
 /**
  * Returns the integer value of the property given by [key].
- * @throws IllegalArgumentException if the property does not exist or the value is not a valid representation of a number.
+ * @throws IllegalArgumentException if the property does not exist, if value is empty or if or the value is not a valid
+ * representation of an integer.
  */
 @JvmName("getIntRequired")
 fun Properties.intRequired(key: String): Int = int(key).require(key)
 
 /**
  * Returns the long value of the property given by [key]
- * or null if property does not exist or if string value is blank.
- * @throws IllegalArgumentException if the value is not a valid representation of a number.
+ * or null if property does not exist or if value is blank.
+ * @throws IllegalArgumentException if the value is not a valid representation of a Long.
  */
 @JvmName("getLong")
 fun Properties.long(key: String): Long? =
@@ -72,14 +72,15 @@ fun Properties.long(key: String): Long? =
 
 /**
  * Returns the long value of the property given by [key].
- * @throws IllegalArgumentException if the property does not exist or the value is not a valid representation of a number.
+ * @throws IllegalArgumentException if the property does not exist, if value is empty or if the value is not a valid
+ * representation of a Long.
  */
 @JvmName("getLongRequired")
 fun Properties.longRequired(key: String): Long = long(key).require(key)
 
 /**
  * Returns `true` if the content of the property given by [key]
- * is equal to the word "true", ignoring case, and `false` otherwise.
+ * is equal to the word "true" (ignoring case) or `false` otherwise.
  * Returns null if property is missing or value is blank.
  */
 @JvmName("getBoolean")
@@ -90,7 +91,7 @@ fun Properties.boolean(key: String): Boolean? =
 
 /**
  * Returns `true` if the content of the property given by [key]
- * is equal to the word "true", ignoring case, and `false` otherwise.
+ * is equal to the word "true" (ignoring case) or `false` otherwise.
  * @throws IllegalArgumentException if the property is missing or value is blank.
  */
 @JvmName("getBooleanRequired")
